95b58d2576d79784ff8fd663805f63fbac7fa317
dandy.hairawan <dandy.hairawan@wgs.co.id>
2013-07-29 16:53:45 +0700

    home and login page :arrow_up:

--- www/js/main.js
+++ www/js/main.js
@@ -6,40 +6,41 @@ var mainapp = {
     } else {
       alert(title ? (title + ": " + message) : message);
     }
-	},
+  },
 
-	renderHomeView: function() {
+  renderHomeView: function() {
     var html =
-            "<div class='header'><h1>Home</h1></div>" +
-            "<div><p>ini adalah home indexnya</p>" +
-            "<br /><p><a id='openReg' href='#'>Open Register</a>" +
-            "</div>"
+    "<div class='header'><h1>Home</h1></div>" +
+    "<div><p>Hello World!</p><br />" +
+    "<a id='openReg' href='#' data-role='button' data-icon='star'>Open Register</a>" +
+    "</div>"
     $('body').html(html);
-	},
+    $('#openReg').bind('click', $.proxy(this.renderRegisterView, this));
+  },
 
   renderRegisterView: function(){
     var html = 
-            "<div class='header'><h1>Register</h1></div>" +
-            "<form action='_self' method ='post'>" +
-            "<label>Name</label>" +
-            "<input type='text' name='username' id='username'/><br />" + 
-            "<label>Email</label>" +
-            "<input type='text'/><br />" + 
-            "<label>Password</label>" +
-            "<input type='password' name='password' id='password'/><br />" + 
-            "<label>Password Confirmation</label>" +
-            "<input type='password' name='passconfirm' id='passconfirm'/><br />" + 
-            "<input type='submit' value='submit'/>" +
-            "</form>" +
-            "<br /> <p><a class='back' href='#'>Go Home</a>"
+    "<div class='header'><h1>Register</h1></div>" +
+    "<form action='_self' method ='post'>" +
+    "<label>Name</label>" +
+    "<input type='text' name='username' id='username'/><br />" + 
+    "<label>Email</label>" +
+    "<input type='text'/><br />" + 
+    "<label>Password</label>" +
+    "<input type='password' name='password' id='password'/><br />" + 
+    "<label>Password Confirmation</label>" +
+    "<input type='password' name='passconfirm' id='passconfirm'/><br />" + 
+    "<input type='submit' value='submit'/>" +
+    "</form>" +
+    "<br /> <p><a class='back' href='#'>Go Home</a>"
     $('body').html(html);
     $('.back').bind('click', $.proxy(this.renderHomeView, this));
   },
 
-	initialize: function() {
+  initialize: function() {
     this.renderHomeView();
-    $('#openReg').bind('click', $.proxy(this.renderRegisterView, this));
-	}
+    // $('#openReg').bind('click', $.proxy(this.renderRegisterView, this));
+  }
 }
 
-mainapp.initialize();
+// mainapp.initialize();
